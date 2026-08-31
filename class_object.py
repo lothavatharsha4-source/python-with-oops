f4d556efae88ff1a0e2c85c5d85ce7526489a9d8
lothavatharsha4-source <lothavatharsha4@gmail.com>
2026-08-31 19:52:59 +0530

    Initial commit

---
+++ class_object.py
@@ -0,0 +1,12 @@
+class student:
+    name = "harsha"
+    age = 19
+    def reading(self):
+        print("student is reading")
+    def writing(self):
+            print("student is writing")
+s1= student()
+print(s1.name)
+print(s1.age)
+s1.reading()
+s1.writing()
