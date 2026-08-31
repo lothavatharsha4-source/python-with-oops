7e6cbdd1e53d0446a2e5dbb79c7000e36ce011c0
lothavatharsha4-source <lothavatharsha4@gmail.com>
2026-08-31 20:26:55 +0530

    Add latest Python OOP file

---
+++ class_multiple_methods.py
@@ -0,0 +1,14 @@
+class student:
+    def name(self):
+        print("Name: Harshavardhan")
+        
+    def age(self):
+        print("Age: 19")
+
+    def course(self):
+        print("course: BE cse")
+
+s1 = student()
+s1.name()
+s1.age()
+s1.course()
